26605d825989cb799450ee451ab833ed0e7159ab
Lydia-Lu-code <guipopo5@gmail.com>
2021-02-08 17:14:36 +0800

    Initial Commit

---
+++ 1A2B-2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  1A2B-2
+//
+//  Created by 維衣 on 2021/2/8.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
